91f8e377fa5e1b7d927a26810792e6a7a3a1bc39
hongxiao <hongtao.xiao@unb.ca>
2018-03-06 14:55:31 -0400

    add and use StatefulWidget in build(); add and use State class

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,31 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
+          title: new Text('Flutter App by Hong Xiao'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords()
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
